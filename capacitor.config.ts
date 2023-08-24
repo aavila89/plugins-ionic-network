d9a5d6cf4f290d9d548e01b2f3ad6db543fde40a
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-24 19:29:55 -0400

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.network.events',
   appName: 'base',
   webDir: 'www',
   server: {
